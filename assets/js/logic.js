40c77b73d35fa022d9ffad9cc0bb8675b976d883
Johnathan <johnathan.mnn@gmail.com>
2022-07-07 23:02:16 -0500

    Introduction to quiz succesfully disappears on click of start game button

---
+++ assets/js/logic.js
@@ -0,0 +1,7 @@
+console.log(window);
+var startButton = document.querySelector('#start');
+
+
+startButton.addEventListener("click", function() {
+    document.getElementById('introduction').style.display="none";
+  });
