24caa134a5be07f4c3dd2494c425a9938cdd62f3
Johnathan <johnathan.mnn@gmail.com>
2022-07-07 22:37:17 -0500

    Introduction to game complete

---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+
+var questions = [
+    {
+        questionText: "Questions",
+        choices: ["Choice", "Choice", "Choice", "Choice"],
+        answer: "Answer",
+    },
+    {
+        questionText: "Questions2",
+        choices: ["Choice2", "Choice2", "Choice2", "Choice2"],
+        answer: "Answer2",
+    },
+    {
+        questionText: "Questions3",
+        choices: ["Choice3", "Choice3", "Choice3", "Choice3"],
+        answer: "Answer3",
+    }
+]
