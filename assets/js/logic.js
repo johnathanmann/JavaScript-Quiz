f980662c438ee03f740e5f5cb554da4fac74a4e8
Johnathan <johnathan.mnn@gmail.com>
2022-07-08 22:29:00 -0500

    First two questions working as desired, but I still havent added the timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,18 +5,18 @@ const questions = document.querySelector('#questions');
 const correctAnswer = document.querySelector('.correctAnswer');
 const incorrectAnswer = document.querySelector('.incorrectAnswer');
 const nextQuestion = document.querySelector('.continue');
+const hiddenCorrect = document.querySelector('.hiddenCorrect');
+const hiddenIncorrect = document.querySelector('.hiddenIncorrect');
 var score = 0;
 // Var's for each question
 const question1 = document.querySelector('#question1');
 const question2 = document.querySelector('#question2');
+const hidden1 = document.getElementById('hidden1');
 
 function init(){
   question1.style.display="none";
   nextQuestion.style.display="none";
   question2.style.display="none";
-  // document.querySelectorAll('correctAnswer').style.display="none";
-  // document.querySelectorAll('incorrectAnswer').style.display="none";
-  // document.querySelectorAll('continue').style.display="none";
 }
 
 init();
@@ -35,18 +35,22 @@ var correctButton = function(){
     console.log(score);
     correctAnswer.style.display="block";
     nextQuestion.style.display="inline";
+    hiddenCorrect.classList.remove('hidden');
 };
 
 var incorrectButton = function(){
   incorrectAnswer.style.display="block";
   nextQuestion.style.display="inline";
+  nextQuestion.style.display="inline";
+  hiddenIncorrect.classList.remove('hidden');
 };
 
 var displayQuestion2 = function (){
+  hiddenCorrect.classList.add('hidden');
+  hiddenIncorrect.classList.add('hidden');
   question1.style.display="none";
   question2.style.display="block";
-  correctAnswer.style.display="none";
   incorrectAnswer.style.display="none";
-  nextQuestion.style.display="none";
 }
+
 
