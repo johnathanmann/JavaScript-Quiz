6960c3ae209c19d256d3700349566b9706da7c4d
Johnathan <johnathan.mnn@gmail.com>
2022-07-08 21:41:41 -0500

    Question 1 can now switch to question 2

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,30 +1,52 @@
+console.log(window);
 // Pertinent variables
-var startGame = document.querySelector('#start');
-const question = document.querySelector('#question');
-const answer1 = document.querySelector('#answer1');
-const answer2 = document.querySelector('#answer2');
-const answer3 = document.querySelector('#answer3');
-const answer4 = document.querySelector('#answer4');
-var correct = document.querySelector('#correct');
+const startGame = document.querySelector('#start');
+const questions = document.querySelector('#questions');
+const correctAnswer = document.querySelector('.correctAnswer');
+const incorrectAnswer = document.querySelector('.incorrectAnswer');
+const nextQuestion = document.querySelector('.continue');
 var score = 0;
+// Var's for each question
+const question1 = document.querySelector('#question1');
+const question2 = document.querySelector('#question2');
 
 function init(){
-  document.getElementById('question1').style.display="none";
-  document.getElementById('correctAnswer').style.display="none";
-  document.getElementById('incorrectAnswer').style.display="none";
+  question1.style.display="none";
+  nextQuestion.style.display="none";
+  question2.style.display="none";
+  // document.querySelectorAll('correctAnswer').style.display="none";
+  // document.querySelectorAll('incorrectAnswer').style.display="none";
+  // document.querySelectorAll('continue').style.display="none";
 }
 
 init();
 
 startGame.addEventListener("click", function() {
     document.getElementById('introduction').style.display="none";
-    document.getElementById('question1').style.display="block";
+    question1.style.display="block";
+    correctAnswer.style.display="none";
+    incorrectAnswer.style.display="none";
+    nextQuestion.style.display="none";
   });
 
 
-correct.addEventListener('click', function(){
+var correctButton = function(){
     score = score + 10;
     console.log(score);
-    document.getElementById('correct').style.backgroundColor="#d36464";
-})
+    correctAnswer.style.display="block";
+    nextQuestion.style.display="inline";
+};
+
+var incorrectButton = function(){
+  incorrectAnswer.style.display="block";
+  nextQuestion.style.display="inline";
+};
+
+var displayQuestion2 = function (){
+  question1.style.display="none";
+  question2.style.display="block";
+  correctAnswer.style.display="none";
+  incorrectAnswer.style.display="none";
+  nextQuestion.style.display="none";
+}
 
