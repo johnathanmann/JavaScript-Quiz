4453bda567833d1190bd4e1e3dbfd456d451e1cc
Johnathan <johnathan.mnn@gmail.com>
2022-07-09 23:06:52 -0500

    All questions and timer working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,8 +2,10 @@ console.log(window);
 // Pertinent variables
 const startGame = document.querySelector('#start');
 const questions = document.querySelector('#questions');
-const nextQuestion = document.querySelector('.continue');
 var score = 0;
+var setInterval;
+var countdown = document.querySelector('#countdown');
+var countdownTimer;
 // Const's for each question and their answers
 const question1 = document.querySelector('#question1');
 const question2 = document.querySelector('#question2');
@@ -18,26 +20,51 @@ const question4 = document.querySelector('#question4');
 // Question 3 results
 const hiddenCorrect3 = document.querySelector('#hiddenCorrect3');
 const hiddenIncorrect3 = document.querySelector('#hiddenIncorrect3');
+const question5 = document.querySelector('#question5');
+// Question 4 results
+const hiddenCorrect4 = document.querySelector('#hiddenCorrect4');
+const hiddenIncorrect4 = document.querySelector('#hiddenIncorrect4');
 
 function init(){
   question1.style.display="none";
   question2.style.display="none";
   question3.style.display="none";
   question4.style.display="none";
+  question5.style.display="none";
 }
 
 init();
 
+// Display question 1
 startGame.addEventListener("click", function() {
     document.getElementById('introduction').style.display="none";
     question1.style.display="block";
+    countdownTimer();
   });
 
+  function countdownTimer() {
+    var timeLeft = 5;
+    secondsLeft= 60;
+    var timeInterval = setInterval(function () {
+      secondsLeft--;
+      countdown.textContent = secondsLeft;
+  
+        if(secondsLeft === 0) {
+  
+          clearInterval(timeInterval);
+          displayMessage();
+        }
+    
+      }, 1000);
+    };
+
+  // Display question 2
 var displayQuestionCorrect2 = function (){
   hiddenCorrect1.classList.remove('hidden');
   hiddenIncorrect1.classList.add('hidden');
   question1.style.display="none";
   question2.style.display="block";
+  score = score + 10;
 }
 
 var displayQuestionIncorrect2 = function (){
@@ -45,13 +72,16 @@ var displayQuestionIncorrect2 = function (){
   hiddenIncorrect1.classList.remove('hidden');
   question1.style.display="none";
   question2.style.display="block";
+  secondsLeft = secondsLeft - 10;
 }
 
+// Display question 3
 var displayQuestionCorrect3 = function (){
   hiddenCorrect2.classList.remove('hidden');
   hiddenIncorrect2.classList.add('hidden');
   question2.style.display="none";
   question3.style.display="block";
+  score = score + 10;
 }
 
 var displayQuestionIncorrect3 = function (){
@@ -59,13 +89,16 @@ var displayQuestionIncorrect3 = function (){
   hiddenIncorrect2.classList.remove('hidden');
   question2.style.display="none";
   question3.style.display="block";
+  secondsLeft = secondsLeft - 10;
 }
 
+// Display question 4
 var displayQuestionCorrect4 = function (){
   hiddenCorrect3.classList.remove('hidden');
   hiddenIncorrect3.classList.add('hidden');
   question3.style.display="none";
   question4.style.display="block";
+  score = score + 10;
 }
 
 var displayQuestionIncorrect4 = function (){
@@ -73,6 +106,24 @@ var displayQuestionIncorrect4 = function (){
   hiddenIncorrect3.classList.remove('hidden');
   question3.style.display="none";
   question4.style.display="block";
+  secondsLeft = secondsLeft - 10;
+}
+
+// Display question 5
+var displayQuestionCorrect5 = function (){
+  hiddenCorrect4.classList.remove('hidden');
+  hiddenIncorrect4.classList.add('hidden');
+  question4.style.display="none";
+  question5.style.display="block";
+  score = score + 10;
+}
+
+var displayQuestionIncorrect5 = function (){
+  hiddenCorrect4.classList.add('hidden');
+  hiddenIncorrect4.classList.remove('hidden');
+  question4.style.display="none";
+  question5.style.display="block";
+  secondsLeft = secondsLeft - 10;
 }
 
 
